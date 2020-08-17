90cf3325aaf552637d820f283e6500118f025bdb
Vineet R. Shenoy <vineetrshenoy@gmail.com>
2020-08-17 10:35:41 -0400

    vc:73: changes to config for cam 14 and 15

--- src/bezier_matching.py
+++ src/bezier_matching.py
@@ -298,7 +298,7 @@ class BezierMatching:
         files = glob.glob(os.path.join(tracker_dir, 'Track*'))
 
         for tracker_file in files:
-            #tracker_file = 'src/vc_outputs/aicity/tracker_output/cam_5_dawn/Track_Car_cam_5_dawn.pkl'
+            #tracker_file = 'src/vc_outputs/rcnn/tracker_output/cam_15/Track_Car_cam_15.pkl'
             with open(tracker_file, 'rb') as f:              
                 
                 tracker_results = pickle.load(f)
