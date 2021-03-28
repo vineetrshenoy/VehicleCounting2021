2d21d9763f1421456cfc5d4e041e5d32c097085e
Vineet R. Shenoy <vineetrshenoy@gmail.com>
2021-03-28 09:51:37 -0400

    vc-35: Detection code successfully integrated

--- src/cttrack.py
+++ src/cttrack.py
@@ -2,6 +2,10 @@ import sys
 import configparser
 import app_logger
 import os
+import cv2
+import time
+import numpy as np
+import pickle
 
 CENTERTRACK_PATH = '/fs/diva-scratch/vshenoy/VehicleCounting/src/CenterTrack/src/lib'
 sys.path.insert(0, CENTERTRACK_PATH)
@@ -14,9 +18,11 @@ config.read(sys.argv[1])
 basic_config = configparser.ConfigParser()
 basic_config.read('config/basic.ini')
 
+from helper import Helper
 from detector import Detector
 from opts import opts
 from CenterTrack.src.demo import demo
+from ctdetection import CTDetection
 
 
 class CTTrack:
@@ -24,53 +30,58 @@ class CTTrack:
     def __init__(self):
 
         self.basic = basic_config['DEFAULT']
-        self.config = config['TRACKING']
+        self.det_conf = config['DETECTION']
+        self.tr_conf = config['TRACKING']
         self.default = config['DEFAULT']
-        self.cam_ident = self.default['cam_name']
+
+        self.ctdets = CTDetection()
+
+        self.video = os.path.join(self.ctdets.basic['data_dir'], 
+            self.ctdets.default['cam_name']) + '.mp4'
+
         
-        self.out_dir = os.path.join(self.basic['output_dir'], 
-            self.basic['job_name'], 'tracker_output', self.cam_ident) #set output directory
-        os.makedirs(self.out_dir, exist_ok=True) #Create tracker_output folder
-
-        opt = self.build_opts()
-
-        demo(opt)
-
-
-    def build_opts(self):
-
-        task = 'tracking'
-        load_model = 'src/CenterTrack/models/coco_tracking.pth'
-        demo = '/fs/diva-scratch/nvidia_data_2020/NVIDIA_AIC_2020_Datasets/AIC20_track1/Dataset_A/cam_13.mp4'
-        video_h = 1080
-        video_w = 1920
-        save_framerate = 10
-        track_thresh = 0.3
-        max_age = 5
-
-        opt_str = 'cam10.ini --task {} --load_model {} --demo {} --resize_video --video_h {} --video_w {} --save_framerate {} --save_video --save_results --track_thresh {} --max_age {}'.format(
-                task,
-                load_model,
-                demo,
-                video_h,
-                video_w,
-                save_framerate,
-                track_thresh,
-                max_age
-            ).split(' ')
         
-        opt = opts().init(opt_str)
 
-        return opt
             
+    def workflow(self):
+        #CTHelper.mask_outputs()
+        cap = cv2.VideoCapture(self.video)
+        
+        detection_dict = {}
+        frame_num = 1
+        print('Starting Detections')
 
 
+        start_process_time = time.time()
+
+        while (cap.isOpened()):
+            
+            ret, frame = cap.read() #Read the frame
+            if ret == False:
+                break
+            
+            ######################### Detection
+            img = self.ctdets.get_model_input(frame)
+            ret = self.ctdets.detector.run(img)
+            dets, all_dets = self.ctdets.mask_outputs(ret['results'])
+            detection_dict[frame_num] = dets
+            #########################
+        
+            print('Frame Num: {}'.format(frame_num))
+            frame_num +=1
+
+
+        outfile = os.path.join(self.ctdets.out_dir, 
+            self.default['cam_name'] + '.pkl' )
+
+        with open(outfile, 'wb') as handle:
+            pickle.dump(detection_dict, handle)
 
 
 
 if __name__ == '__main__':
     
-    CTTrack()
+    CTTrack().workflow()
 
     #opt = opts().init()
     #demo(opt)
