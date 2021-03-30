639b9df0598db89e2f43bc39e698cc8a36968eb5
Vineet R. Shenoy <vineetrshenoy@gmail.com>
2021-03-30 09:31:53 -0400

    vc-38: Fixing max_age problem in multitracker.py; Done

--- src/detection_tracker.py
+++ src/detection_tracker.py
@@ -81,8 +81,8 @@ class DetectionTracker:
             if frame_num % 200 == 0:
                 print('Frame Number {}'.format(frame_num))
                 self.tracker.write_outputs()
-                self.counter.workflow()
-                #self.tracker.flush()
+                #self.counter.workflow()
+                self.tracker.flush()
             
             frame_num += 1
         
@@ -98,7 +98,7 @@ class DetectionTracker:
             pickle.dump(detection_dict, handle)
 
         self.tracker.write_outputs()
-        self.counter.workflow()
+        #self.counter.workflow()
         self.counter.track1txt.close()
         '''
         pid = subprocess.Popen([sys.executable, "bezier_online.py config/cam_13.ini"], 
